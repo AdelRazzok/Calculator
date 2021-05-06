00b2f0226cfb552ee4e4dccb2ee582a38aef0d59
Delino <64547069+d3lino@users.noreply.github.com>
2021-05-06 09:31:06 +0100

    Calculatrice fonctionnelle

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,32 +3,33 @@ const allNumber = document.querySelectorAll('button[data-button="number"]');
 const allOperator = document.querySelectorAll('button[data-button="operator"]');
 const cancelBtn = document.querySelector('button[data-button="cancel"]');
 const equalBtn = document.querySelector('button[data-button="equal"]') ;
-const calcDetail = document.getElementById("calcDetail");
-const result = document.getElementById("result");
-
+const comma = document.querySelector('button[data-button="comma"]');
 
 /* INSTRUCTIONS */
 allNumber.forEach(element => {
     element.addEventListener("click", function() {
-        // console.log(this.innerHTML);
         calcDetail.innerHTML += this.innerHTML;
     });
 });
 
 allOperator.forEach(element => {
     element.addEventListener("click", function() {
-        // console.log(this.innerHTML);
         calcDetail.innerHTML += this.dataset.op;
     });
 });
 
+comma.addEventListener("click", function() {
+    calcDetail.innerHTML += this.dataset.op;
+});
+
 equalBtn.addEventListener("click", () => {
-    // console.log(calcDetail);
+    equalSign.innerHTML = "=";
     result.innerHTML = eval(calcDetail.innerHTML);
 });
 
 // Touche C : supprime tout ce qu'on a entré
 cancelBtn.addEventListener("click", () => {
     calcDetail.innerHTML = "";
+    equalSign.innerHTML = "";
     result.innerHTML = "";
 });
