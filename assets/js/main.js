a7a1bc2bcd4511779e52cc3db362e533e7a60ec1
Delino <64547069+d3lino@users.noreply.github.com>
2022-01-25 15:33:51 +0100

    minor fixes, useless ids deleted, js functions improved

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,41 @@
-/* VARIABLES */
 const allNumber = document.querySelectorAll('button[data-button="number"]');
 const allOperator = document.querySelectorAll('button[data-button="operator"]');
 const cancelBtn = document.querySelector('button[data-button="cancel"]');
 const equalBtn = document.querySelector('button[data-button="equal"]') ;
 const comma = document.querySelector('button[data-button="comma"]');
 
-/* INSTRUCTIONS */
 allNumber.forEach(element => {
     element.addEventListener("click", function() {
-        calcDetail.innerHTML += this.innerHTML;
+        calcDetail.innerText += this.innerText;
+		allOperator.forEach(element => {
+			element.disabled = false;
+		});
     });
 });
 
 allOperator.forEach(element => {
     element.addEventListener("click", function() {
-        calcDetail.innerHTML += this.dataset.op;
+        calcDetail.innerText += this.dataset.op;
+		allOperator.forEach(element => {
+			element.disabled = true;
+		});
     });
 });
 
 comma.addEventListener("click", function() {
-    calcDetail.innerHTML += this.dataset.op;
+    calcDetail.innerText += this.dataset.op;
 });
 
-equalBtn.addEventListener("click", () => {
-    equalSign.innerHTML = "=";
-    result.innerHTML = eval(calcDetail.innerHTML);
+equalBtn.addEventListener("click", function() {
+    equalSign.innerText = "=";
+    result.innerText = eval(calcDetail.innerText);
 });
 
-// Touche C : supprime tout ce qu'on a entré
-cancelBtn.addEventListener("click", () => {
-    calcDetail.innerHTML = "";
-    equalSign.innerHTML = "";
-    result.innerHTML = "";
+cancelBtn.addEventListener("click", function() {
+    calcDetail.innerText = "";
+    equalSign.innerText = "";
+    result.innerText = "";
+	allOperator.forEach(element => {
+		element.disabled = false;
+	});
 });
